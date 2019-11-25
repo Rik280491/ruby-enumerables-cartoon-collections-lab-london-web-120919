0b8d8d8de0c91aa4009ca38d6a95ac82b2588f03
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-11-25 16:12:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map{|p| puts "{p}!"}
   while i < planeteer_calls.length do 
-    
+    planeteer_calls[i] = planeteer_calls[i].upcase
+    idx += 1
 end
 
 def long_planeteer_calls# code an argument here
